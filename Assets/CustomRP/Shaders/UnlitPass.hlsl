3f83c64d321de4bef7be4d55ea2cb6d1a1c8f5ed
Sibyl-null <1286438810@qq.com>
2024-04-17 23:18:44 +0800

    [feat]: UnlitPass  shade

---
+++ Assets/CustomRP/Shaders/UnlitPass.hlsl
@@ -0,0 +1,13 @@
+﻿#ifndef CUSTOM_UNLIT_PASS_INCLUDED
+#define CUSTOM_UNLIT_PASS_INCLUDED
+
+void UnlitPassVertex()
+{
+}
+
+float4 UnlitPassFragment()
+{
+    return float4(1, 1, 1, 1);
+}
+
+#endif // CUSTOM_UNLIT_PASS_INCLUDED
