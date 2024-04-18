19b4af657d02bbcfc2cbb3eca55da9ae2725463d
Sibyl-null <1286438810@qq.com>
2024-04-18 23:13:52 +0800

    [feat]: 使用 SRP 核心库

--- Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
+++ Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
@@ -1,7 +1,16 @@
 ﻿#ifndef CUSTOM_UNITY_INPUT_INCLUDED
 #define CUSTOM_UNITY_INPUT_INCLUDED
 
-float4x4 unity_ObjectToWorld;       // 当前模型矩阵
-float4x4 unity_MatrixVP;            // 当前视图 * 投影矩阵
+float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;  
+
+float4x4 unity_MatrixVP;                // 视图 * 投影矩阵
+float4x4 unity_MatrixV;                 // 视图矩阵
+float4x4 unity_MatrixInvV;              // 逆视图矩阵
+float4x4 unity_prev_MatrixM;            // 上一帧的模型矩阵
+float4x4 unity_prev_MatrixIM;           // 上一帧的逆模型矩阵
+float4x4 glstate_matrix_projection;     // 投影矩阵
+
+real4 unity_WorldTransformParams;
 
 #endif // CUSTOM_UNITY_INPUT_INCLUDED
