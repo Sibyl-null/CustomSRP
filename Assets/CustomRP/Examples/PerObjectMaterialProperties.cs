a345b5b4d3fd9874b053df490e5ef14a122310b9
Sibyl-null <1286438810@qq.com>
2024-04-21 23:34:20 +0800

    [feat]: Cutoff C# Set

--- Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -6,24 +6,27 @@ namespace CustomRP.Examples
     public class PerObjectMaterialProperties : MonoBehaviour
     {
         private static readonly int BaseColorId = Shader.PropertyToID("_BaseColor");
+        private static readonly int CutoffId = Shader.PropertyToID("_Cutoff");
         private static MaterialPropertyBlock _propertyBlock;
         
         [SerializeField] private Color _baseColor = Color.white;
+        [SerializeField, Range(0, 1)] private float _cutoff = 0.5f;
 
         private void OnValidate()
         {
-            SetColor();
+            Setup();
         }
 
         private void Awake()
         {
-            SetColor();
+            Setup();
         }
 
-        private void SetColor()
+        private void Setup()
         {
             _propertyBlock ??= new MaterialPropertyBlock();
             _propertyBlock.SetColor(BaseColorId, _baseColor);
+            _propertyBlock.SetFloat(CutoffId, _cutoff);
             GetComponent<Renderer>().SetPropertyBlock(_propertyBlock);
         }
     }
