7d8144cb304edbc0ea7165f3371f9b520eb4db80
Sibyl-null <1286438810@qq.com>
2024-04-23 22:38:00 +0800

    [feat]: 光照漫反射项

--- Assets/CustomRP/ShaderLibrary/Light.hlsl
+++ Assets/CustomRP/ShaderLibrary/Light.hlsl
@@ -15,4 +15,21 @@ Light GetDirectionalLight()
     return light;
 }
 
+/** 光照颜色计算 */
+float3 IncomingLight(Surface surface, Light light)
+{
+    // 漫反射项
+    return saturate(dot(surface.normal, light.direction)) * light.color;
+}
+
+float3 GetLighting(Surface surface, Light light)
+{
+    return IncomingLight(surface, light) * surface.color;
+}
+
+float3 GetLighting(Surface surface)
+{
+    return GetLighting(surface, GetDirectionalLight());
+}
+
 #endif // CUSTOM_LIGHT_INCLUDED
