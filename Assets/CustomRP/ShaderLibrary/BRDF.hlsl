8ec1b2fdd28842515b43413c6ed8e971d9e10c0c
yangboyuan <yangboyuan@dailyinnovation.biz>
2024-04-25 18:35:17 +0800

    [feat]: 反射率应用

--- Assets/CustomRP/ShaderLibrary/BRDF.hlsl
+++ Assets/CustomRP/ShaderLibrary/BRDF.hlsl
@@ -1,17 +1,28 @@
 #ifndef CUSTOM_BRDF_INCLUDED
 #define CUSTOM_BRDF_INCLUDED
 
+#define MIN_REFLECTIVITY 0.04
+
 struct BRDF
 {
-    float3 diffuse;          // 漫反射颜色
-    float3 specular;         // 镜面反射颜色
+    float3 diffuse;          // 发散颜色
+    float3 specular;         // 反射颜色
     float roughness;         // 粗糙度
 };
 
+/** 计算发散率，1 - 发射率，能量守恒 */
+float OneMinusReflectivity(float metallic)
+{
+    float range = 1 - MIN_REFLECTIVITY;
+    return (1 - metallic) * range;
+}
+
 BRDF GetBRDF(Surface surface)
 {
+    float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
+    
     BRDF brdf;
-    brdf.diffuse = surface.color;
+    brdf.diffuse = surface.color * oneMinusReflectivity;
     brdf.specular = 0;
     brdf.roughness = 1;
     return brdf;
--- Assets/CustomRP/ShaderLibrary/Lighting.hlsl
+++ Assets/CustomRP/ShaderLibrary/Lighting.hlsl
@@ -1,10 +1,8 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
-/** 光照颜色计算 */
 float3 IncomingLight(Surface surface, Light light)
 {
-    // 漫反射项
     return saturate(dot(surface.normal, light.direction)) * light.color;
 }
 
