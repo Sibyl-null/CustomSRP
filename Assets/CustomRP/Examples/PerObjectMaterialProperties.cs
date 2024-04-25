edaf980ce67797424661ed1edf60111563ab8378
yangboyuan <yangboyuan@dailyinnovation.biz>
2024-04-25 12:23:29 +0800

    [feat]: metallic、smoothness 表面属性定义

--- Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -7,10 +7,14 @@ namespace CustomRP.Examples
     {
         private static readonly int BaseColorId = Shader.PropertyToID("_BaseColor");
         private static readonly int CutoffId = Shader.PropertyToID("_Cutoff");
+        private static readonly int MetallicId = Shader.PropertyToID("_Metallic");
+        private static readonly int SmoothnessId = Shader.PropertyToID("_Smoothness");
         private static MaterialPropertyBlock _propertyBlock;
         
         [SerializeField] private Color _baseColor = Color.white;
         [SerializeField, Range(0, 1)] private float _cutoff = 0.5f;
+        [SerializeField, Range(0, 1)] private float _metallic = 0f;
+        [SerializeField, Range(0, 1)] private float _smoothness = 0.5f;
 
         private void OnValidate()
         {
@@ -27,6 +31,8 @@ namespace CustomRP.Examples
             _propertyBlock ??= new MaterialPropertyBlock();
             _propertyBlock.SetColor(BaseColorId, _baseColor);
             _propertyBlock.SetFloat(CutoffId, _cutoff);
+            _propertyBlock.SetFloat(MetallicId, _metallic);
+            _propertyBlock.SetFloat(SmoothnessId, _smoothness);
             GetComponent<Renderer>().SetPropertyBlock(_propertyBlock);
         }
     }
