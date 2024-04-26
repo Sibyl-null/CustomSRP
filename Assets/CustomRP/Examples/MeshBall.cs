0464745f13455410d22e35232f1cc0409d955334
yangboyuan <yangboyuan@dailyinnovation.biz>
2024-04-26 10:48:08 +0800

    [feat]: MeshBall 支持金属、光滑度属性

--- Assets/CustomRP/Examples/MeshBall.cs
+++ Assets/CustomRP/Examples/MeshBall.cs
@@ -6,6 +6,8 @@ namespace CustomRP.Examples
     {
         private static readonly int BaseColorId = Shader.PropertyToID("_BaseColor");
         private static readonly int CutoffId = Shader.PropertyToID("_Cutoff");
+        private static readonly int MetallicId = Shader.PropertyToID("_Metallic");
+        private static readonly int SmoothnessId = Shader.PropertyToID("_Smoothness");
         
         [SerializeField] private Mesh _mesh;
         [SerializeField] private Material _material;
@@ -14,12 +16,16 @@ namespace CustomRP.Examples
         private Matrix4x4[] _matrices;
         private Vector4[] _colors;
         private float[] _cutoffs;
+        private float[] _metallic;
+        private float[] _smoothness;
 
         private void Awake()
         {
             _matrices = new Matrix4x4[1023];
             _colors = new Vector4[1023];
             _cutoffs = new float[1023];
+            _metallic = new float[1023];
+            _smoothness = new float[1023];
 
             for (int i = 0; i < _matrices.Length; i++)
             {
@@ -29,6 +35,9 @@ namespace CustomRP.Examples
                 
                 _colors[i] = new Vector4(Random.value, Random.value, Random.value, 1);
                 _cutoffs[i] = Random.Range(0.3f, 0.8f);
+                
+                _metallic[i] = Random.value < 0.25f ? 1f : 0f;
+                _smoothness[i] = Random.Range(0.05f, 0.95f);
             }
         }
 
@@ -39,6 +48,8 @@ namespace CustomRP.Examples
                 _propertyBlock = new MaterialPropertyBlock();
                 _propertyBlock.SetVectorArray(BaseColorId, _colors);
                 _propertyBlock.SetFloatArray(CutoffId, _cutoffs);
+                _propertyBlock.SetFloatArray(MetallicId, _metallic);
+                _propertyBlock.SetFloatArray(SmoothnessId, _smoothness);
             }
 
             Graphics.DrawMeshInstanced(_mesh, 0, _material, _matrices, _matrices.Length, _propertyBlock);
