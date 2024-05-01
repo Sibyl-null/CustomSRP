6a196ed346e2b4b4417bdb772b4e1ac85a9a0162
Sibyl-null <1286438810@qq.com>
2024-05-01 17:02:52 +0800

    [feat]: 阴影级联设置项

--- Assets/CustomRP/Runtime/ShadowSettings.cs
+++ Assets/CustomRP/Runtime/ShadowSettings.cs
@@ -20,10 +20,25 @@ namespace CustomRP.Runtime
         {
             // 使用单个纹理来包含多个阴影贴图
             public TextureSize atlasSize;
+            
+            // 阴影级联设置
+            [Range(1, 4)] public int cascadesCount;
+            [Range(0f, 1f)] public float cascadeRatios1;
+            [Range(0f, 1f)] public float cascadeRatios2;
+            [Range(0f, 1f)] public float cascadeRatios3;
+            
+            public Vector3 CascadeRatios => new Vector3(cascadeRatios1, cascadeRatios2, cascadeRatios3);
         }
         
         [Min(0f)]
         public float maxDistance = 100f;
-        public Directional directional = new() { atlasSize = TextureSize._1024 };
+        public Directional directional = new()
+        {
+            atlasSize = TextureSize._1024,
+            cascadesCount = 4,
+            cascadeRatios1 = 0.1f,
+            cascadeRatios2 = 0.25f,
+            cascadeRatios3 = 0.5f
+        };
     }
 }
