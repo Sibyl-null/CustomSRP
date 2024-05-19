6f7ad916446db3abd9c43e41564a7711960642a7
Sibyl-null <1286438810@qq.com>
2024-05-19 17:40:38 +0800

    [feat]: 光照贴图采样

--- Assets/CustomRP/ShaderLibrary/GI.hlsl
+++ Assets/CustomRP/ShaderLibrary/GI.hlsl
@@ -1,6 +1,11 @@
 #ifndef CUSTOM_GI_INCLUDED
 #define CUSTOM_GI_INCLUDED
 
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/EntityLighting.hlsl"
+
+TEXTURE2D(unity_Lightmap);
+SAMPLER(sampler_unity_Lightmap);
+
 #ifdef LIGHTMAP_ON
     #define GI_ATTRIBUTE_DATA float2 lightMapUV : TEXCOORD1;
     #define GI_VARYINGS_DATA float2 lightMapUV : VAR_LIGHT_MAP_UV;
@@ -19,10 +24,30 @@ struct GI
     float3 diffuse;
 };
 
+float3 SampleLightMap(float2 lightMapUV)
+{
+#ifdef LIGHTMAP_ON
+    #ifdef UNITY_LIGHTMAP_FULL_HDR
+        bool compress = false;
+    #else
+        bool compress = true;
+    #endif
+    
+    return SampleSingleLightmap(
+        TEXTURE2D_ARGS(unity_Lightmap, sampler_unity_Lightmap),
+        lightMapUV,
+        float4(1, 1, 0, 0),
+        compress,
+        float4(LIGHTMAP_HDR_MULTIPLIER, LIGHTMAP_HDR_EXPONENT, 0, 0));
+#else
+    return 0.0;
+#endif // LIGHTMAP_ON
+}
+
 GI GetGI(float2 lightMapUV)
 {
     GI gi;
-    gi.diffuse = float3(lightMapUV, 0);
+    gi.diffuse = SampleLightMap(lightMapUV);
     return gi;
 }
 
